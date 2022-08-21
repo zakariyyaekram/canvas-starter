d38a148d208f1c7b8c246f3b8a2dfb5e4dada582
theimpostor <sahirhoda@gmail.com>
2022-08-20 20:36:54 -0500

    misc

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 c.fillStyle = 'black';
 c.strokeRect(0, 0, 500, 400);
+
 c.fillStyle = 'blue';
 c.fillRect(10, 10, 480, 380);
+
 c.fillStyle = 'orange';
-c.font = 'small-caps bold 24px/1 sans-serif';
+c.font = 'small-caps bold 24px sans-serif';
 c.textAlign = 'center';
 c.fillText('Hello code hackerz!', 250, 200);
